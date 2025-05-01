17874f0375ba3a169acca4ccf2c7e4ce18c7dac3
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-05-01 16:14:34 +0200

    chore: integrated bloc and completed theme toggle

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -2,10 +2,12 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotifyclone/common/widgets/button/basic_app_button.dart';
 import 'package:spotifyclone/core/configs/assets/app_images.dart';
 import 'package:spotifyclone/core/configs/assets/app_vectors.dart';
+import 'package:spotifyclone/presentation/choose_mode/bloc/theme_cubit.dart';
 
 class ChooseModePage extends StatelessWidget {
   const ChooseModePage({super.key});
@@ -22,128 +24,140 @@ class ChooseModePage extends StatelessWidget {
             ),
             decoration: const BoxDecoration(
                 image: DecorationImage(
-                    fit: BoxFit.fill, image: AssetImage(AppImages.chooseModeBG))),
+                    fit: BoxFit.fill,
+                    image: AssetImage(AppImages.chooseModeBG))),
           ),
-
           Container(color: Colors.black.withOpacity(0.15)),
-
           Padding(
             padding: const EdgeInsets.symmetric(
               vertical: 40,
               horizontal: 40,
             ),
             child: Column(
-                children: [
-                  Align(
-                      alignment: Alignment.topCenter,
-                      child: SvgPicture.asset(AppVectors.logo)),
-                  const Spacer(),
-                  const Text(
-                    'Choose Mode',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
+              children: [
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.logo)),
+                const Spacer(),
+                const Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
                   ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        children: [
-                          ClipOval(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(
-                                sigmaX: 10,
-                                sigmaY: 10,
-                              ),
-                              child: Container(
-                                width: 80,
-                                height: 80,
-                                decoration: BoxDecoration(
-                                  color: Color(0xff30393C).withOpacity(0.5),
-                                  shape: BoxShape.circle,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            context
+                                .read<ThemeCubit>()
+                                .updateTheme(ThemeMode.light);
+                          },
+                          child: GestureDetector(
+                            onTap: () {
+                              context
+                                  .read<ThemeCubit>()
+                                  .updateTheme(ThemeMode.dark);
+                            },
+                            child: ClipOval(
+                              child: BackdropFilter(
+                                filter: ImageFilter.blur(
+                                  sigmaX: 10,
+                                  sigmaY: 10,
                                 ),
-                                child: SvgPicture.asset(
-                                  AppVectors.moonIcon,
-                                  fit: BoxFit.none,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          const Text(
-                            'Dark Mode',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 14,
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 40,
-                      ),
-                      Column(
-                        children: [
-                          ClipOval(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(
-                                sigmaX: 10,
-                                sigmaY: 10,
-                              ),
-                              child: Container(
-                                width: 80,
-                                height: 80,
-                                decoration: BoxDecoration(
-                                  color: Color(0xff30393C).withOpacity(0.5),
-                                  shape: BoxShape.circle,
-                                ),
-                                child: SvgPicture.asset(
-                                  AppVectors.sunIcon,
-                                  fit: BoxFit.none,
+                                child: Container(
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xff30393C).withOpacity(0.5),
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: SvgPicture.asset(
+                                    AppVectors.moonIcon,
+                                    fit: BoxFit.none,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          const Text(
-                            'Light Mode',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 14,
-                            ),
-                          )
-                        ],
-                      ),
-
-                    ],
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  BasicAppButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => const ChooseModePage()
                         ),
-                      );
-                    },
-                    title: 'Continue',
-                  ),
-                ],
-              ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text(
+                          'Dark Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(
+                              sigmaX: 10,
+                              sigmaY: 10,
+                            ),
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                color: Color(0xff30393C).withOpacity(0.5),
+                                shape: BoxShape.circle,
+                              ),
+                              child: SvgPicture.asset(
+                                AppVectors.sunIcon,
+                                fit: BoxFit.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text(
+                          'Light Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const ChooseModePage()),
+                    );
+                  },
+                  title: 'Continue',
+                ),
+              ],
+            ),
           ),
         ],
       ),
